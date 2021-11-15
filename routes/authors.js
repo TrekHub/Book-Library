242cc4584622e36552b02abec1ee9c2fbc85d6dd
TrekHub <teddyomondi001@gmail.com>
2021-11-15 12:09:48 +0300

    add the create and find author routes

--- routes/authors.js
+++ routes/authors.js
@@ -5,20 +5,57 @@ const Author = require('../models/author')
 
 
 //All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new  RegExp(req.query.name, 'i')
+    }
+
+
+    try {
+        const allAuthors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            allAuthors: allAuthors,
+            searchOptions: req.query
+        })
+
+    } catch {
+        res.redirect('/')
+
+    }
+
 });
 
 //New Authors Route 
 router.get('/new', (req, res) => {
-    res.render('authors/new', {author: new Author()})
+    res.render('authors/new', {
+        author: new Author()
+    })
 });
 
 //Create Author Route 
-router.post('/', (req, res) => {
-    res.send(req.body.name)
-    
+router.post('/', async (req, res) => {
+
+    const author = new Author({
+        name: req.body.name
+    });
+
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+
+    }
+
+
 });
 
 
-module.exports = router
+module.exports = router;
