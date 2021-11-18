3e940c6f853b79548bae1a4e4b2c5b1ce4155649
TrekHub <teddyomondi001@gmail.com>
2021-11-18 17:10:22 +0300

    add the addBooks functionality

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book');
 const Author = require('../models/author');
 const multer = require('multer');
+const {
+    query
+} = require('express');
 const imageMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const upload = multer({
@@ -18,8 +22,30 @@ const upload = multer({
 
 
 // All books route
-router.get('/', (req, res) => {
-    res.render('books/index')
+router.get('/', async (req, res) => {
+
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishedDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishedDate', req.query.publishedAfter)
+    }
+
+    try {
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        })
+
+    } catch {
+        res.redirect('/')
+    }
 
 });
 
@@ -34,8 +60,8 @@ router.get('/new', async (req, res) => {
 router.post('/', upload.single('cover'), async (req, res) => {
 
 
-    const fileName = req.file != null ? req.file.filename : 
-    null
+    const fileName = req.file != null ? req.file.filename :
+        null
 
     const book = await new Book({
         title: req.body.title,
@@ -55,11 +81,22 @@ router.post('/', upload.single('cover'), async (req, res) => {
 
     } catch {
 
+        if (book.coverImageName != null) {
+            removeBookCover(book.coverImageName)
+        }
+
         renderNewPage(res, book, true)
     }
 
 
 });
+
+function removeBookCover(fileName) {
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if (err) console.error(err)
+
+    })
+}
 
 
 async function renderNewPage(res, book, hasError = false) {
